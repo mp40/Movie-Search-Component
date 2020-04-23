2ead0243f1d368619b26e3a1ff78b0582986dbc3
Matthew Philps <philps.matthew@gmail.com>
2020-04-23 18:44:32 +0900

    added react create app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <div>Movie Search App</div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
