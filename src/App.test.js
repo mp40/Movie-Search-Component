0aea05373b8721e024d59040c7a674bc4269ac14
Matthew Philps <philps.matthew@gmail.com>
2020-04-23 18:57:59 +0900

    linter configured

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-describe('wiring test', () => {
-const wrapper = shallow(<App/>)
-  it('should work',() => {
-    expect(wrapper.text()).toContain('Movie Search App');
+describe("wiring test", () => {
+  const wrapper = shallow(<App />);
+  it("should work", () => {
+    expect(wrapper.text()).toContain("Movie Search App");
   });
 });
