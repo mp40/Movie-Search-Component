b2b9bfd75ff925eae4541cb46da974e4029b9931
Matthew Philps <philps.matthew@gmail.com>
2020-04-25 11:28:06 +0900

    fetch in place

--- src/fetch/fetch.spec.js
+++ src/fetch/fetch.spec.js
@@ -1,24 +1,56 @@
-import fetchTrending from './index';
+import {
+  fetchTrending, fetchSearchResults, fetchActorData, getFilteredResults,
+} from './index';
+
 import payload from '../fixtures/payload.json';
+import actorPayload from '../fixtures/actorPayload.json';
+import output from '../fixtures/output.json';
 
 const API_KEY = process.env.REACT_APP_MOVIE_DATABASE_API_KEY;
 
-describe('fetching trending', () => {
-  const mockSuccessResponse = payload;
-  const mockJsonPromise = Promise.resolve(mockSuccessResponse);
-  const mockFetchPromise = Promise.resolve({
-    json: () => mockJsonPromise,
-  });
+describe('fetching data', () => {
+  const mockFetchPayload = Promise.resolve({ json: () => payload });
+  const mockFetchActorOverview = Promise.resolve({ json: () => actorPayload });
+
   beforeEach(() => {
-    global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+    global.fetch = jest.fn().mockImplementation((arg) => {
+      if (typeof arg === 'number') {
+        return mockFetchActorOverview;
+      }
+      return mockFetchPayload;
+    });
   });
+
   afterEach(() => {
     global.fetch.mockClear();
   });
+
   it('should fetch trending data from API', async () => {
     const data = await fetchTrending();
     expect(global.fetch).toHaveBeenCalledTimes(1);
     expect(global.fetch).toHaveBeenCalledWith(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`);
     expect(data).toEqual(payload);
+  });
+
+  it('should fetch search querry data from API', async () => {
+    const query = 'tiger';
+    const data = await fetchSearchResults(query);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1`);
+    expect(data).toEqual(payload);
+  });
+
+  it('should fetch actor data from API', async () => {
+    const id = 1337;
+    const data = await fetchActorData(id);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(`https://api.themoviedb.org/3/person/${id}?api_key=${API_KEY}&language=en-US`);
+    expect(data).toEqual(payload);
+  });
+
+  it('should get filtered data', async () => {
+    const query = 'tiger';
+    const results = await getFilteredResults(query);
+    expect(results).toEqual(output);
   });
 });
--- src/fetch/index.js
+++ src/fetch/index.js
@@ -1,14 +1,39 @@
 const API_KEY = process.env.REACT_APP_MOVIE_DATABASE_API_KEY;
 
-const fetchTrending = async () => {
-  let response;
-  try {
-    response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`);
-  } catch (err) {
-    console.error(err);
-  }
-  const data = await response.json();
-  return data;
+export const fetchTrending = async () => {
+  const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`);
+  return response.json();
 };
 
-export default fetchTrending;
+export const fetchSearchResults = async (query) => {
+  const response = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1`);
+  return response.json();
+};
+
+export const fetchActorData = async (id) => {
+  const response = await fetch(`https://api.themoviedb.org/3/person/${id}?api_key=${API_KEY}&language=en-US`);
+  return response.json();
+};
+
+export const getFilteredResults = async (query) => {
+  const data = await fetchSearchResults(query);
+
+  return Promise.all(data.results.map(async (item) => {
+    let actorData;
+
+    if (item.media_type === 'person') {
+      actorData = await fetchActorData(item.id);
+    }
+
+    return {
+      id: item.id,
+      overview: item.overview || actorData?.biography,
+      imagePath: item.poster_path || item.profile_path,
+      date: item.release_date || item.first_air_date,
+      name: item.title || item.name,
+      voteAverage: item.vote_average,
+      gender: item.gender,
+      mediaType: item.media_type,
+    };
+  }));
+};
