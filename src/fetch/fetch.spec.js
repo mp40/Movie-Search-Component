704120e3d5c00a891bb799372ae2ae2f979ce4bd
Matthew Philps <philps.matthew@gmail.com>
2020-04-25 19:59:38 +0900

    updated fetch tests

--- src/fetch/fetch.spec.js
+++ src/fetch/fetch.spec.js
@@ -13,53 +13,71 @@ const API_KEY = process.env.REACT_APP_MOVIE_DATABASE_API_KEY;
 
 describe('fetching data', () => {
   const mockFetchPayload = Promise.resolve({ json: () => payload });
-  const mockFetchActorOverview = Promise.resolve({ json: () => actorPayload });
+  const mockFetchActorPayload = Promise.resolve({ json: () => actorPayload });
 
-  beforeEach(() => {
-    global.fetch = jest.fn().mockImplementation((arg) => {
-      if (typeof arg === 'number') {
-        return mockFetchActorOverview;
-      }
-      return mockFetchPayload;
+
+  describe('fetching list', () => {
+    beforeEach(() => {
+      global.fetch = jest.fn().mockImplementation(() => mockFetchPayload);
+    });
+
+    afterEach(() => {
+      global.fetch.mockClear();
+    });
+
+    it('should fetch trending data from API', async () => {
+      const data = await fetchTrending();
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`,
+      );
+      expect(data).toEqual(payload);
+    });
+
+    it('should fetch search querry data from API', async () => {
+      const query = 'tiger';
+      const data = await fetchSearchResults(query);
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(
+        `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1`,
+      );
+      expect(data).toEqual(payload);
     });
   });
 
-  afterEach(() => {
-    global.fetch.mockClear();
+  describe('fetching person data', () => {
+    beforeEach(() => {
+      global.fetch = jest.fn().mockImplementation(() => mockFetchActorPayload);
+    });
+
+    afterEach(() => {
+      global.fetch.mockClear();
+    });
+    it('should fetch actor data from API', async () => {
+      const id = 1337;
+      const data = await fetchActorData(id);
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(
+        `https://api.themoviedb.org/3/person/${id}?api_key=${API_KEY}&language=en-US`,
+      );
+      expect(data).toEqual(actorPayload);
+    });
   });
 
-  it('should fetch trending data from API', async () => {
-    const data = await fetchTrending();
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
-    );
-    expect(data).toEqual(payload);
-  });
+  describe('geting filtered list', () => {
+    beforeEach(() => {
+      global.fetch = jest.fn(() => mockFetchActorPayload)
+        .mockImplementationOnce(() => mockFetchPayload);
+    });
 
-  it('should fetch search querry data from API', async () => {
-    const query = 'tiger';
-    const data = await fetchSearchResults(query);
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(
-      `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1`
-    );
-    expect(data).toEqual(payload);
-  });
+    afterEach(() => {
+      global.fetch.mockClear();
+    });
 
-  it('should fetch actor data from API', async () => {
-    const id = 1337;
-    const data = await fetchActorData(id);
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(
-      `https://api.themoviedb.org/3/person/${id}?api_key=${API_KEY}&language=en-US`
-    );
-    expect(data).toEqual(payload);
-  });
-
-  it('should get filtered data', async () => {
-    const query = 'tiger';
-    const results = await getFilteredResults(query);
-    expect(results).toEqual(output);
+    it('should get filtered data', async () => {
+      const query = 'tiger';
+      const results = await getFilteredResults(query);
+      expect(results).toEqual(output);
+    });
   });
 });
