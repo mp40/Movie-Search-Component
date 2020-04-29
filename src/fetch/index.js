cfafef8e7c4b1b2e1a6f500d90237a5538543499
Matthew Philps <philps.matthew@gmail.com>
2020-04-30 08:47:28 +0900

    linting

--- src/fetch/index.js
+++ src/fetch/index.js
@@ -14,65 +14,63 @@ export const fetchTrending = async () => {
 
 export const fetchSearchResults = async (query) => {
   const response = await fetch(
-    `${API_SEARCH_URL}${API_KEY}&language=en-US&query=${query}&page=1`
+    `${API_SEARCH_URL}${API_KEY}&language=en-US&query=${query}&page=1`,
   );
   return response.json();
 };
 
 export const fetchActorData = async (id) => {
   const response = await fetch(
-    `${API_ACTOR_URL}${id}${API_KEY}&language=en-US`
+    `${API_ACTOR_URL}${id}${API_KEY}&language=en-US`,
   );
   return response.json();
 };
 
 export const fetchVideoData = async (mediaType, id) => {
   const response = await fetch(
-    `${API_BASE_URL}/${mediaType}/${id}/videos${API_KEY}&language=en-US`
+    `${API_BASE_URL}/${mediaType}/${id}/videos${API_KEY}&language=en-US`,
   );
   return response.json();
 };
 
-const fetchAdditionalData = (data) => {
-  return Promise.all(
-    data.results.map(async (item) => {
-      let actorData;
-      let videoData;
-      let trailerData;
+const fetchAdditionalData = (data) => Promise.all(
+  data.results.map(async (item) => {
+    let actorData;
+    let videoData;
+    let trailerData;
 
-      if (item.media_type === 'person') {
-        actorData = await fetchActorData(item.id);
-      }
+    if (item.media_type === 'person') {
+      actorData = await fetchActorData(item.id);
+    }
 
-      if (item.media_type !== 'person') {
-        videoData = await fetchVideoData(item.media_type, item.id);
-        trailerData = videoData?.results.reduce((filtered, video) => {
-          if (video.type === 'Trailer' && video.site === 'YouTube') {
-            filtered.push({
-              id: video.id,
-              key: video.key,
-              site: video.site,
-              size: video.size,
-            });
-          }
-          return filtered;
-        }, []);
-      }
+    if (item.media_type !== 'person') {
+      videoData = await fetchVideoData(item.media_type, item.id);
+      trailerData = videoData?.results.reduce((filtered, video) => {
+        if (video.type === 'Trailer' && video.site === 'YouTube') {
+          filtered.push({
+            id: video.id,
+            key: video.key,
+            site: video.site,
+            size: video.size,
+          });
+        }
+        return filtered;
+      }, []);
+    }
 
-      return {
-        id: item.id,
-        overview: item.overview || actorData?.biography,
-        imagePath: item.poster_path || item.profile_path,
-        date: item.release_date || item.first_air_date,
-        name: item.title || item.name,
-        voteAverage: item.vote_average,
-        trailers: trailerData || undefined,
-        gender: item.gender,
-        mediaType: item.media_type,
-      };
-    })
-  );
-};
+    return {
+      id: item.id,
+      overview: item.overview || actorData?.biography,
+      imagePath: item.poster_path || item.profile_path,
+      date: item.release_date || item.first_air_date,
+      name: item.title || item.name,
+      voteAverage: item.vote_average,
+      trailers: trailerData || undefined,
+      gender: item.gender,
+      mediaType: item.media_type,
+    };
+  }),
+);
 
 export const getFilteredResults = async (query) => {
   const data = await fetchSearchResults(query);
